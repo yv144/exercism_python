89708e21662101927893127e26f5b34970ad0bd7
yv144 <yevgeniy.lukhmanov@gmail.com>
2024-09-17 22:23:57 +0500

    solved

--- Easy/List Ops.py
+++ Easy/List Ops.py
@@ -1,38 +1,39 @@
 # Implement basic list operations.
 
-def append(list1, list2):
+def append(list1, list2) -> list:
     return list1 + list2
 
-def concat(lists):
+def concat(lists) -> list:
     base = []
     for item in lists:
         base += item
     return base
 
-def filter(function, list):
+def filter(function, list) -> list:
     base = []
     for item in list:
-        if function(item):
-            base += item
+        if function(item): 
+            base += [item]
     return base
 
-x = filter(lambda x: x % 2 == 1, [1, 2, 3, 5])
+def length(list) -> int:
+    count  = 0
+    for _ in list:
+        count += 1
+    return count
 
-def length(list):
-    pass
+def map(function, list) -> list:
+    return [function(item) for item in list]
+        
+def foldl(function, list, initial) -> float|int:
+    for item in list:
+        initial = function(initial,item)
+    return initial
 
+def foldr(function, list, initial) -> float|int:
+    for item in list[::-1]:
+        initial = function(initial,item)
+    return initial
 
-def map(function, list):
-    pass
-
-
-def foldl(function, list, initial):
-    pass
-
-
-def foldr(function, list, initial):
-    pass
-
-
-def reverse(list):
-    pass
+def reverse(list) -> list:
+    return list[::-1]
