8499571cd2e1e94eed7c4c855f1d6a26f4e5b609
yv144 <yevgeniy.lukhmanov@gmail.com>
2024-09-20 20:29:36 +0500

    solved

---
+++ Easy/Prime Factors.py
@@ -0,0 +1,14 @@
+# Compute the prime factors of a given natural number.
+
+def factors(start_value: int) -> list[int]:
+    
+    output = []
+    while start_value >1:
+        for number in range(2,start_value+1):
+            print(f'{number=}')
+            print(f'{start_value=}')
+            if start_value % number == 0:
+                start_value = start_value // number
+                output.append(number)
+                break
+    return output
