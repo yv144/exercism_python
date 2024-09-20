2e5704098cc46c85c8244ec7106984ce15327c09
yv144 <yevgeniy.lukhmanov@gmail.com>
2024-09-20 20:30:35 +0500

    solved

--- Easy/Prime Factors.py
+++ Easy/Prime Factors.py
@@ -5,8 +5,6 @@ def factors(start_value: int) -> list[int]:
     output = []
     while start_value >1:
         for number in range(2,start_value+1):
-            print(f'{number=}')
-            print(f'{start_value=}')
             if start_value % number == 0:
                 start_value = start_value // number
                 output.append(number)
